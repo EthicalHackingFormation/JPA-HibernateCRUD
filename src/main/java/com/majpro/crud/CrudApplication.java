d9c6e5258949b389bbaaf70e4ca80037a537adb4
EthicalHackingFormation <44448220+EthicalHackingFormation@users.noreply.github.com>
2024-04-10 17:11:57 +0300

    second commit

--- src/main/java/com/majpro/crud/CrudApplication.java
+++ src/main/java/com/majpro/crud/CrudApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.majpro.crud.dao.StudentDAO;
+import com.majpro.crud.entity.Student;
+
 @SpringBootApplication
 public class CrudApplication {
 
@@ -13,9 +16,40 @@ public class CrudApplication {
 	}
     
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
            return runner -> {
-			  System.out.println("Hello World!");
+			  //createStudent(studentDAO);
+			  createMultipleStudent(studentDAO);
 		   };
+	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+
+	     //create the student object
+         System.out.println("Creating 03 new student object....");
+		 Student tempStudent1 = new Student("John","Wick","artistwick@gmail.com");
+		 Student tempStudent2 = new Student("Paul","Phoenix","paul@gmail.com");
+		 Student tempStudent3 = new Student("Nav","ix","navix@gmail.com");
+
+		//save the student object
+		System.out.println("Saving the 03 students.....");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+ 
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		
+		//create the student object
+         System.out.println("Creating new student object....");
+		 Student tempStudent = new Student("Paul","Doe","artist@gmail.com");
+		
+		 //save the student object
+		 System.out.println("Saving the student.....");
+         studentDAO.save(tempStudent);
+
+		//display id of the saved student
+		System.out.println("Saved student.Generated id : "+tempStudent.getId());
 	}
 }
---
+++ src/main/java/com/majpro/crud/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.majpro.crud.dao;
+
+import com.majpro.crud.entity.Student;
+
+public interface StudentDAO {
+    
+    void save(Student theStudent);
+}
